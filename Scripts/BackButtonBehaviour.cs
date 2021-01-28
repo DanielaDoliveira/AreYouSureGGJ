5ba56941083a743e55cd720256cba479cde8f50d
fialho <danidaninpce3@gmail.com>
2021-01-28 14:44:54 -0300

    First menu changes

---
+++ Scripts/BackButtonBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackButtonBehaviour : MonoBehaviour
+{
+    public void btnBack()
+    {
+        SceneManager.LoadScene(0);//ACESSA A TELA DE USER INTERFACE
+    }
+
+    
+}
---
+++ Scripts/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UI : MonoBehaviour,IUserInterface
+{
+   
+    private Scene scene; 
+    public void btnPlay()
+    {
+     
+         SceneManager.LoadScene(2);//Acessa a tela 2, que é o dia 1
+        
+    }
+    public void btnCredits()
+    {
+        SceneManager.LoadScene(1);//Acessa a tela 1(CREDITS)
+
+    }
+   
+    public void btnQuit()
+    {
+        Application.Quit();//SAI DO JOGO
+    }
+}
