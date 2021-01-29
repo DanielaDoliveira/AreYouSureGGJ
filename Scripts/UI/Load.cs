abf5185cb7da7091f4a0b9232f853d29e9fd81b5
fialho <danidaninpce3@gmail.com>
2021-01-28 22:02:17 -0300

    Adding introduction animation

---
+++ Scripts/UI/Load.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Load : MonoBehaviour
+{
+  
+    public string sceneToLoad;
+    public float fixedTime = 3;
+    
+    void Start()
+    {
+        StartCoroutine(LoadSceneinFixedTime(sceneToLoad));  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //espera um tempo para iniciar outra cena
+    public IEnumerator LoadSceneinFixedTime(string scene)
+    {
+       yield return new WaitForSeconds(fixedTime);
+       SceneManager.LoadScene(scene);
+    }
+}
---
+++ Scripts/UI/TextAnimationOrder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextAnimationOrder : MonoBehaviour
+{
+  
+    public GameObject globalGO;
+    public Text teamLogo;
+    public Animator anim;
+    void Start()
+    {
+        teamLogo.text = " ";
+         StartCoroutine("GlobalGameJamLogo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    IEnumerator teamLogoAnim()
+    {
+         
+         teamLogo.text = "O RESTO TEAM";
+         yield return new WaitForSeconds(0.5f);
+        anim.SetTrigger("fade");
+    }
+       
+   public IEnumerator GlobalGameJamLogo()
+    {
+        globalGO.SetActive(true);
+        yield return new WaitForSeconds(3);
+        globalGO.SetActive(false);
+        StartCoroutine(teamLogoAnim());
+    }
+}
